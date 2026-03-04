909736d029c1952c4f6129e4db8fcbf362c7359a
David-Jimenez-Gon264 <20253rd001@utez.edu.mx>
2026-03-04 13:35:08 -0600

    YA NO TIENE ERRORES We

--- Alumno.java
+++ Alumno.java
@@ -1,13 +1,37 @@
 public class Alumno {
-
     String nombre, matricula;
-    double[] calificaion = new double[3];
+    double cal1, cal2, cal3, promedio;
 
-    public void setCalificacion(){
+
+    public Alumno (String nombre, String matricula){
+        this.nombre = nombre;
+        this.matricula = matricula;
+    }
+
+    public void setCalificacion(double cal1, double cal2, double cal3){
+        this.cal1 = cal1;
+        this.cal2 = cal2;
+        this.cal3 = cal3;
 
     }
 
-    public
+    public void calcularPromedio(){
+        double promedio = (cal1 + cal2 +cal3)/3;
+        this.promedio = promedio;
+    }
+    
+    public void mostrarAlumno(){
+        System.out.print("NOMBRE: "+this.nombre);
+        System.out.print("MATRICULA: "+this.nombre);
+        System.out.print("CALIFICACION 1: "+this.cal1+"\nCALIFICACION 2: "+this.cal2+"\nCALIFICACION 3: "+this.cal3);
+        System.out.print("TU PROMEDIO: "+this.promedio);
+    }
+
+
+
+
+
+   
 
 
 
